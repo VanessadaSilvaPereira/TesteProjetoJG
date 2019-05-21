75a13ea751ae3bd276e2aab07f0d31839c32f715
Vanessa <vanessa.pereira787@gmail.com>
2019-05-21 11:44:01 -0300

    criados os casos de teste

---
+++ src/CampoAssuntoVazio.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CampoAssuntoVazio {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/EmailIncorreto.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EmailIncorreto {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
